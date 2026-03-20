fc6348e2ba63e3ddfc9448f87e721e19ae1bf190
Pengo Wray <me@pengowray.com>
2026-03-20 13:47:08 +1100

    android minSdkVersion: 26 (for cpal) android sdk 21 to 25 no longer supported

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "com.batmonic.app"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
